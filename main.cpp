0111c49673bfae05fe67b841c5c7da0fb267b318
OpVir <timurirnazar785s@gmail.com>
2025-11-05 22:40:30 +0300

    Init Commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iostream> // std::{cout, endl}
+#include <cstdlib>  // std::system
+#include "contiguous_iterator.hpp"
+#include "list.hpp"
+int main()
+{
+  box::list<int> a;
+  std::system("pause");
+  return 0;
+}
